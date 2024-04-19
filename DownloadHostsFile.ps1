19730e218d05063f490a515ec2b6f056abda95c3
firebird5589 <167464393+firebird5589@users.noreply.github.com>
2024-04-19 21:34:15 +0100

    Update DownloadHostsFile.ps1

--- DownloadHostsFile.ps1
+++ DownloadHostsFile.ps1
@@ -1,3 +1,3 @@
-invoke-WebRequest -Uri https://raw.githubusercontent.com/firebird5589/VariousBatchFiles/main/hosts -OutFile D:\Downloads\hosts
+invoke-WebRequest -Uri https://raw.githubusercontent.com/firebird5589/VariousBatchFiles/main/hosts -OutFile D:\hosts
 
 Move-Item -path D:\hosts -Destination D:\Documents
