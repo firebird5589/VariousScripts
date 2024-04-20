4f1933788d2aeace245ce025b44f4044c380639b
firebird5589 <167464393+firebird5589@users.noreply.github.com>
2024-04-20 14:13:46 +0100

    Update DownloadHostsFile.ps1

--- DownloadHostsFile.ps1
+++ DownloadHostsFile.ps1
@@ -1,8 +1,1 @@
-# Download the file
-$sourceUri = "https://raw.githubusercontent.com/firebird5589/VariousBatchFiles/main/hosts"
-$downloadPath = "D:\"
-Invoke-WebRequest -Uri $sourceUri -OutFile $downloadPath
-
-# Move the file
-$destinationPath = "D:\Documents"
-Move-Item -Path $downloadPath -Destination $destinationPath
+invoke-WebRequest -Uri https://raw.githubusercontent.com/firebird5589/VariousBatchFiles/main/hosts -OutFile C:\Windows\System32\drivers\etc\hosts
