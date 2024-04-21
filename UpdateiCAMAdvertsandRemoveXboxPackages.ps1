defea6f954fd3b0e539594902bf7fb0a709590b3
firebird5589 <167464393+firebird5589@users.noreply.github.com>
2024-04-21 20:36:52 +0100

    Update UpdateiCAMAdvertsandRemoveXboxPackages.ps1

--- UpdateiCAMAdvertsandRemoveXboxPackages.ps1
+++ UpdateiCAMAdvertsandRemoveXboxPackages.ps1
@@ -8,13 +8,13 @@ regedit D:\Documents\ApplyingiCAMAdverts.reg
 dism /Online /Get-ProvisionedAppxPackages | `Select-String PackageName | `Select-String xbox | `ForEach-Object {$_.Line.Split(':')[1].Trim()} | `ForEach-Object { dism /Online /Remove-ProvisionedAppxPackage /PackageName:$_}
 
 #5 seconds pause
-Start-Sleep -Seconds 5
+#Start-Sleep -Seconds 5
 
 #Search for packages with the name "xbox" and removes for all users accounts
 Get-ProvisionedAppxPackage -Online | `Where-Object { $_.PackageName -match "xbox"} | `ForEach-Object { Remove-ProvisionedAppxPackage -Online -AllUsers -PackageName $} 
 
 #5 seconds pause
-Start-Sleep -Seconds 5
+#Start-Sleep -Seconds 5
 
 #Removes the Xbox Gaminng Overlay package for all users accounts
 Get-AppxPackage -AllUsers -PackageTypeFilter Bundle -Name "*Microsoft.XboxGamingOverlay*" | Remove-AppxPackage -AllUsers 
