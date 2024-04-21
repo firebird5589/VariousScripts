f54632bf8d20a6aea23215dd7bf2b40376cd48f0
firebird5589 <167464393+firebird5589@users.noreply.github.com>
2024-04-21 19:56:03 +0100

    Update UpdateiCAMAdvertsandRemoveXboxPackages.ps1

--- UpdateiCAMAdvertsandRemoveXboxPackages.ps1
+++ UpdateiCAMAdvertsandRemoveXboxPackages.ps1
@@ -1,17 +1,23 @@
- #Applying .reg file that sets the Adverts profile in iCAMConfig
- regedit /s C:\Users\Administrator\Desktop\ApplyingiCAMAdverts.reg
+#Download iCAM reg file from my github repo and save to D:\Documents folder
+invoke-WebRequest -Uri https://raw.githubusercontent.com/firebird5589/RegFiles/main/ApplyingiCAMAdverts.reg -OutFile D:\Documents\ApplyingiCAMAdverts.reg
 
- #Search for all packages with the Name "xbox" and removes them
- dism /Online /Get-ProvisionedAppxPackages | `Select-String PackageName | `Select-String xbox | `ForEach-Object {$_.Line.Split(':')[1].Trim()} | `ForEach-Object { dism /Online /Remove-ProvisionedAppxPackage /PackageName:$_}
+#Applying .reg file that sets the Adverts profile in iCAMConfig
+regedit D:\Documents\ApplyingiCAMAdverts.reg
 
- #5 seconds pause
- Start-Sleep -Seconds 5
+#Search for all packages with the Name "xbox" and removes them
+dism /Online /Get-ProvisionedAppxPackages | `Select-String PackageName | `Select-String xbox | `ForEach-Object {$_.Line.Split(':')[1].Trim()} | `ForEach-Object { dism /Online /Remove-ProvisionedAppxPackage /PackageName:$_}
 
- #Search for packages with the name "xbox" and removes them
- Get-ProvisionedAppxPackage -Online | `Where-Object { $_.PackageName -match "xbox"} | `ForEach-Object { Remove-ProvisionedAppxPackage -Online -AllUsers -PackageName $} 
+#5 seconds pause
+Start-Sleep -Seconds 5
 
-  #5 seconds pause
- Start-Sleep -Seconds 5
+#Search for packages with the name "xbox" and removes them
+Get-ProvisionedAppxPackage -Online | `Where-Object { $_.PackageName -match "xbox"} | `ForEach-Object { Remove-ProvisionedAppxPackage -Online -AllUsers -PackageName $} 
 
- #Removes the Xbox Gaminng Overlay package for all users accounts
- Get-AppxPackage -AllUsers -PackageTypeFilter Bundle -Name "*Microsoft.XboxGamingOverlay*" | Remove-AppxPackage -AllUsers 
+#5 seconds pause
+Start-Sleep -Seconds 5
+
+#Removes the Xbox Gaminng Overlay package for all users accounts
+Get-AppxPackage -AllUsers -PackageTypeFilter Bundle -Name "*Microsoft.XboxGamingOverlay*" | Remove-AppxPackage -AllUsers 
+
+#Delete reg file from D:\Documents folder
+Remove-Item -path D:\Documents\ApplyingiCAMAdverts.reg
