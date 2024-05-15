a97265296c7c326f471177ac09a54fb21dcdddf8
firebird5589 <167464393+firebird5589@users.noreply.github.com>
2024-05-15 20:59:57 +0100

    Update UpdateiCAMPrintClient.ps1

--- UpdateiCAMPrintClient.ps1
+++ UpdateiCAMPrintClient.ps1
@@ -13,5 +13,5 @@ Start-Process msiexec.exe -Wait -ArgumentList '/I D:\Downloads\iCAM_Print_Client
 #Delete iCAM Printer Control v5.0.1.0 from D:\Downloads folder
 Remove-Item -path D:\Downloads\iCAM_Print_Client_5.0.1.0.msi
 
-#Restart PC (Just to make sure everything applies correctly after install - MSI installer does not action this by default)
+#Restart PC (Just to make sure everything applies correctly after the install - MSI installer does not action this by default)
 Restart-Computer -Force
