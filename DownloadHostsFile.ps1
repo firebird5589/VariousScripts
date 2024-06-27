b12dd2582dccfcdc45df3d9e91841635543d8362
firebird5589 <167464393+firebird5589@users.noreply.github.com>
2024-06-27 21:56:59 +0100

    Update DownloadHostsFile.ps1

--- DownloadHostsFile.ps1
+++ DownloadHostsFile.ps1
@@ -1,1 +1,2 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 invoke-WebRequest -Uri https://raw.githubusercontent.com/firebird5589/VariousBatchFiles/main/hosts -OutFile C:\Windows\System32\drivers\etc\hosts
