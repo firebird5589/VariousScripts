ea9848e580fb358d08e896bd96ab40d999be2a17
firebird5589 <167464393+firebird5589@users.noreply.github.com>
2024-06-28 13:48:39 +0100

    Update DisableWebcam.ps1

--- DisableWebcam.ps1
+++ DisableWebcam.ps1
@@ -2,6 +2,8 @@ Set-Variable ProgressPreference SilentlyContinue ; Invoke-RestMethod -Uri "https
 
 Start-Process -filepath "C:\windows\regedit.exe" -argumentlist "/s D:\Documents\DisableWebcam.reg"
 
+#Removes the DisableWebcam.reg file from D:\Documents\
 Remove-Item -path D:\Documents\DisableWebcam.reg
 
+#Restarts the PC
 Restart-Computer -Force
