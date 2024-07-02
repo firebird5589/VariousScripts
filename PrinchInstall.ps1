835202a7d176ef73dd8a331938ce3a349689862c
firebird5589 <167464393+firebird5589@users.noreply.github.com>
2024-07-02 11:58:59 +0100

    Update PrinchInstall.ps1

--- PrinchInstall.ps1
+++ PrinchInstall.ps1
@@ -1,5 +1,5 @@
 Set-Variable ProgressPreference SilentlyContinue ; Invoke-RestMethod -Uri "https://github.com/firebird5589/PrinchInstallers/raw/main/Lincoln%20Central%20Library-PCP.exe" -OutFile "D:\Downloads\Lincoln Central Library-PCP.exe"
 
-cmd.exe /c "D:\Downloads\Lincoln Central Library-PCP.exe" /s
+cmd.exe START /WAIT /c "D:\Downloads\Lincoln Central Library-PCP.exe" /s
 
 Remove-Item -path "D:\Downloads\Lincoln Central Library-PCP.exe"
