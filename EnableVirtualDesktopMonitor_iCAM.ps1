862b3d64f85655ee943425a2cd6e2face3c7ad73
firebird5589 <167464393+firebird5589@users.noreply.github.com>
2024-08-07 21:17:10 +0100

    Update EnableVirtualDesktopMonitor_iCAM.ps1

--- EnableVirtualDesktopMonitor_iCAM.ps1
+++ EnableVirtualDesktopMonitor_iCAM.ps1
@@ -1,7 +1,7 @@
-#Download iCAM reg file from my github repo and save to D:\Documents folder
+#Download iCAM reg file from my github repo and saves to D:\Documents folder
 Set-Variable ProgressPreference SilentlyContinue ; Invoke-RestMethod -Uri "https://raw.githubusercontent.com/firebird5589/RegFiles/main/EnableVirtualDesktopMonitor_iCAM.reg" -OutFile D:\Documents\EnableVirtualDesktopMonitor_iCAM.reg
 
-#Applying .reg file that sets the Adverts profile in iCAMConfig
+#Applying .reg file that sets the enable virtual desktop monitor in iCAM
 Start-Process -filepath "C:\windows\regedit.exe" -argumentlist "/s D:\Documents\EnableVirtualDesktopMonitor_iCAM.reg"
 
 #Delete reg file from D:\Documents folder
